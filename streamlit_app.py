f8ec68440ba460e10a28ca06597b64781d97d66f
MVRios <riosmv.96@gmail.com>
2025-10-07 14:31:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from retriever_AGENT import Sabrina, initial_state
+from retriever_AGENT import Sabrina, initial_state, system_message
 from langchain_core.messages import HumanMessage
 
 # Mantener estado del chat
 if "state" not in st.session_state:
     st.session_state.state = initial_state
+
+config = {"configurable": {"thread_id": "1"}}
 
 st.title("Sabrina - Teaching Assistant 🤖")
 
@@ -12,7 +14,11 @@ user_input = st.text_input("You:", "")
 
 if st.button("Send") and user_input:
     st.session_state.state["messages"].append(HumanMessage(content=user_input))
-    response_state = Sabrina.invoke(st.session_state.state)
+    response_state = Sabrina.invoke(
+        st.session_state.state,
+        config=config,
+        stream_mode="values"
+    )
     st.session_state.state = response_state
     ai_msg = response_state["messages"][-1].content
     st.text_area("Sabrina:", value=ai_msg, height=150)
